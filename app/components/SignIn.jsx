84508a0bfff7fffcfaacef7164cfbe3a803ba865
Patrick Akuagwu <patrick@rohealth.ng>
2023-08-03 11:36:32 +0100

    Login

--- app/components/SignIn.jsx
+++ app/components/SignIn.jsx
@@ -4,43 +4,51 @@ import { loginFields } from "../forms/formfield";
 import FormAction from "../forms/formAction";
 import FormExtra from "../forms/formExtra";
 import Input from "../components/Input";
+import axios from "../api/axios";
+import { toast, ToastContainer } from "react-toastify";
 
-const regApi = "https://warehouzitserver.onrender.com/api/v1/auth/register";
+const signinApi = "https://warehouzitserver.onrender.com/api/v1/auth/login";
 const fields = loginFields;
 let fieldsState = {};
 fields.forEach((field) => (fieldsState[field.id] = ""));
 
 export default function SignIn() {
-	const [loginState, setLoginState] = useState(fieldsState);
+    const [signinState, setSigninState] = useState(fieldsState);
 	const [isLoading, setIsLoading] = useState(false);
 
-	const handleChange = (e) => {
-		setLoginState({ ...loginState, [e.target.id]: e.target.value });
-	};
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setSigninState({ ...signinState, [id]: value });
+      };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setIsLoading(true);
-		await login();
-		setIsLoading(false);
-	};
+	  const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        await signin();
+        setIsLoading(false);
+        resetForm();
+      };
+
+      const resetForm = () => {
+        setSigninState(fieldsState);
+      };
 
 
-	async function login() {
-		try {
-			const response = await axios.post(regApi, loginState, {
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			const data = await response.data;
-			console.log(data);
-			toast.success("You have successfully Logged In");
-		} catch (error) {
-			console.error(error);
-			toast.error("Some error has occured");
-		}
-	}
+      async function signin() {
+        try {
+          const response = await axios.post(signinApi, signinState, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const data = await response.data;
+          console.log(data);
+          toast.success("You have successfully signed in");
+        } catch (error) {
+          console.error(error);
+          toast.error("Invalid credentials. Please check your email and password.");
+        }
+      }
 
 	return (
 		<form className="mt-8 space-y-6 w-96 m-auto" onSubmit={handleSubmit}>
@@ -64,6 +72,7 @@ export default function SignIn() {
 
 			<FormExtra />
 			<FormAction handleSubmit={handleSubmit} isLoading={isLoading} text="Log in" />
+            <ToastContainer />
 		</form>
 	);
 }
